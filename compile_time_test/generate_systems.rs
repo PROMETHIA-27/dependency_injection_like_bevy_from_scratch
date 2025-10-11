8bed637e53d280dd1552ce94529fb5b6fe66f788
PROMETHIA <electriccobras@gmail.com>
2025-10-10 20:18:59 -0400

    near completely optimal

--- compile_time_test/generate_systems.rs
+++ compile_time_test/generate_systems.rs
@@ -6,7 +6,10 @@ const COUNT: usize = 5000;
 
 fn main() {
     let args = std::env::args();
-    let call = matches!(args.skip(1).next().as_ref().map(String::as_str), Some("call"));
+    let call = matches!(
+        args.skip(1).next().as_ref().map(String::as_str),
+        Some("call")
+    );
 
     let mut out = String::new();
 
@@ -29,23 +32,20 @@ fn main() {
     println!("{out}");
 }
 
-const TYPES: &[&str] = &[
-    "i32",
-    "usize",
-    "&'static str",
-];
+const TYPES: &[&str] = &["i32", "usize", "&'static str"];
 
 fn generate_header(out: &mut String) {
-    out.push_str("#![allow(unused)]\n");
-    out.push_str("use crate::{Res, Scheduler, SystemParam};\n\n");
+    out.push_str("#![allow(unused, non_camel_case_types)]\n");
+    out.push_str("use crate::{Res, Scheduler, FunkySystem, StoredSystem};\n");
+    out.push_str("use std::{any::{Any, TypeId}, cell::RefCell, collections::HashMap, marker::PhantomData};\n\n");
 }
 
 fn generate_system_adds(out: &mut String) {
     out.push_str("pub fn add_systems(sched: &mut Scheduler) {\n");
 
-    for i in 0..COUNT {
-        out.push_str(&format!("\tsched.add_system(system{i});\n"));
-    }
+    // for i in 0..COUNT {
+    //     out.push_str(&format!("\tsched.add_system(system{i});\n"));
+    // }
 
     out.push_str("}\n\n");
 }
@@ -53,42 +53,68 @@ fn generate_system_adds(out: &mut String) {
 fn generate_system_calls(out: &mut String) {
     out.push_str("pub fn call_systems(sched: &mut Scheduler) {\n");
 
-    out.push_str("\tlet arg0: Res<i32> = Res::retrieve(&sched.resources);\n");
-    out.push_str("\tlet arg1: Res<usize> = Res::retrieve(&sched.resources);\n");
-    out.push_str("\tlet arg2: Res<&'static str> = Res::retrieve(&sched.resources);\n");
-
-    out.push_str("\tlet arg0: &i32 = &arg0;\n");
-    out.push_str("\tlet arg1: &usize = &arg1;\n");
-    out.push_str("\tlet arg2: &str = arg2.clone();\n");
-
     for i in 0..COUNT {
-        out.push_str(&format!("\tsystem{i}(arg0, arg1, arg2);\n"));
+        out.push_str(&format!("\tcall_system{i}(sched);\n"));
     }
 
     out.push_str("}\n\n");
 }
 
 fn generate_system(out: &mut String, index: usize) {
-    out.push_str(&format!("pub fn system{index}("));
+    let arg_count = random::<u8>(..) % 8;
+    // let arg_count = 0;
 
-    let arg_count = random::<u8>() % 8;
+    let mut sys_args = String::new();
+    let mut arg_declares = String::new();
+    let mut arg_passes = String::new();
 
     for i in 0..arg_count {
-        let ty_index = random::<u8>() % TYPES.len() as u8;
+        let ty_index = random::<u8>(..) % TYPES.len() as u8;
         let ty = TYPES[ty_index as usize];
 
-        out.push_str(&format!("_arg{i}: Res<{ty}>, "));
-    }    
+        sys_args.push_str(&format!("_arg{i}: Res<{ty}>, "));
+        arg_declares.push_str(&format!("\t\tlet arg{i} = Res::<{ty}>::get(resources);\n"));
+        arg_passes.push_str(&format!("arg{i}, "));
+    }
 
-    out.push_str(") { println!(\"Hello, world!\"); }\n\n");
+    out.push_str(&format!(
+        "#[allow(non_upper_case_globals)]
+pub static system{index}: StoredSystem = __system_caller{index};
+// pub static system{index}: StoredSystem = &__system_caller{index};
+
+fn __system_caller{index}(resources: &mut HashMap<TypeId, Box<dyn Any>>) {{
+{arg_declares}
+    __system_internal{index}({arg_passes});
+}}
+
+pub fn __system_internal{index}({sys_args}) {{ println!(\"Hello, world!\"); }}\n\n"
+    ));
 }
 
 fn generate_call_system(out: &mut String, index: usize) {
-    out.push_str(&format!("pub fn system{index}("));
-    
-    out.push_str("_arg0: &i32, ");
-    out.push_str("_arg1: &usize, ");
-    out.push_str("_arg2: &str, ");
+    let arg_count = random::<u8>(..) % 8;
 
-    out.push_str(") { println!(\"Hello, world!\"); }\n\n");
+    let mut sys_args = String::new();
+    let mut arg_declares = String::new();
+    let mut arg_passes = String::new();
+
+    for i in 0..arg_count {
+        let ty_index = random::<u8>(..) % TYPES.len() as u8;
+        let ty = TYPES[ty_index as usize];
+
+        sys_args.push_str(&format!("_arg{i}: Res<{ty}>, "));
+        arg_declares.push_str(&format!("\tlet arg{i} = Res::<{ty}>::get(&sched.resources);\n"));
+        arg_passes.push_str(&format!("arg{i}, "));
+    }
+
+    out.push_str(&format!(
+"pub fn call_system{index}(sched: &mut Scheduler) {{
+{arg_declares}
+    system{index}({arg_passes});
+}}
+    
+pub fn system{index}({sys_args}) {{ 
+    println!(\"Hello, world!\"); 
+}}\n\n"
+));
 }
--- compile_time_test/src/main.rs
+++ compile_time_test/src/main.rs
@@ -5,214 +5,225 @@ use std::collections::HashMap;
 use std::marker::PhantomData;
 use std::ops::{Deref, DerefMut};
 
-struct FunctionSystem<Input, F> {
-    f: F,
-    marker: PhantomData<fn() -> Input>,
+// struct FunctionSystem<Input, F> {
+//     f: F,
+//     marker: PhantomData<fn() -> Input>,
+// }
+
+#[derive(Copy, Clone)]
+struct FunkySystem {
+    function: &'static (dyn Fn(&mut HashMap<TypeId, Box<dyn Any>>) + Send + Sync),
 }
 
-trait System {
-    fn run(&mut self, resources: &mut HashMap<TypeId, RefCell<Box<dyn Any>>>);
-}
+// trait System {
+//     fn run(&mut self, resources: &mut HashMap<TypeId, RefCell<i32>>);
+// }
 
-trait IntoSystem<Input> {
-    type System: System;
-
-    fn into_system(self) -> Self::System;
-}
-
-type StoredSystem = Box<dyn System>;
+// type StoredSystem = &'static (dyn Fn(&mut HashMap<TypeId, Box<dyn Any>>) + Send + Sync);
+type StoredSystem = fn(&mut HashMap<TypeId, Box<dyn Any>>);
 
 #[derive(Default)]
 struct Scheduler {
     systems: Vec<StoredSystem>,
-    resources: HashMap<TypeId, RefCell<Box<dyn Any>>>,
+    resources: HashMap<TypeId, Box<dyn Any>>,
 }
 
 impl Scheduler {
     pub fn run(&mut self) {
         for system in self.systems.iter_mut() {
-            system.run(&mut self.resources);
+            system(&mut self.resources);
+            // system.run(&mut self.resources);
         }
     }
 
-    pub fn add_system<I, S: System + 'static>(&mut self, system: impl IntoSystem<I, System = S>) {
-        self.systems.push(Box::new(system.into_system()));
+    pub fn add_system(&mut self, system: StoredSystem) {
+        self.systems.push(system);
     }
 
     pub fn add_resource<R: 'static>(&mut self, res: R) {
-        self.resources
-            .insert(TypeId::of::<R>(), RefCell::new(Box::new(res)));
+        self.resources.insert(TypeId::of::<R>(), Box::new(res));
     }
 }
 
-trait SystemParam {
-    type Item<'new>;
+// trait SystemParam {
+//     type Item<'new>;
 
-    fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r>;
-}
+//     fn retrieve<'r>(resources: &'r HashMap<TypeId, Box<dyn Any>>) -> Self::Item<'r>;
+// }
 
 struct Res<'a, T: 'static> {
-    value: Ref<'a, Box<dyn Any>>,
-    _marker: PhantomData<&'a T>,
+    value: &'a T,
 }
 
-impl<T: 'static> Deref for Res<'_, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        self.value.downcast_ref().unwrap()
-    }
-}
-
-impl<'res, T: 'static> SystemParam for Res<'res, T> {
-    type Item<'new> = Res<'new, T>;
-
-    fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r> {
+impl<T: 'static> Res<'_, T> {
+    fn get(resources: &HashMap<TypeId, Box<dyn Any>>) -> Res<'_, T> {
         Res {
-            value: resources.get(&TypeId::of::<T>()).unwrap().borrow(),
-            _marker: PhantomData,
+            value: resources
+                .get(&TypeId::of::<T>())
+                .unwrap()
+                .downcast_ref()
+                .unwrap(),
         }
     }
 }
 
-struct ResMut<'a, T: 'static> {
-    value: RefMut<'a, Box<dyn Any>>,
-    _marker: PhantomData<&'a mut T>,
-}
+// impl<T: 'static> Deref for Res<'_, T> {
+//     type Target = T;
 
-impl<T: 'static> Deref for ResMut<'_, T> {
-    type Target = T;
+//     fn deref(&self) -> &T {
+//         self.value
+//     }
+// }
 
-    fn deref(&self) -> &T {
-        self.value.downcast_ref().unwrap()
-    }
-}
+// impl<'res, T: 'static> SystemParam for Res<'res, T> {
+//     type Item<'new> = Res<'new, T>;
 
-impl<T: 'static> DerefMut for ResMut<'_, T> {
-    fn deref_mut(&mut self) -> &mut T {
-        self.value.downcast_mut().unwrap()
-    }
-}
+//     fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<i32>>) -> Self::Item<'r> {
+//         Res {
+//             value: resources.get(&TypeId::of::<T>()).unwrap().borrow(),
+//             _marker: PhantomData,
+//         }
+//     }
+// }
 
-impl<'res, T: 'static> SystemParam for ResMut<'res, T> {
-    type Item<'new> = ResMut<'new, T>;
+// struct ResMut<'a, T: 'static> {
+//     value: RefMut<'a, i32>,
+//     _marker: PhantomData<&'a mut T>,
+// }
 
-    fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<Box<dyn Any>>>) -> Self::Item<'r> {
-        ResMut {
-            value: resources.get(&TypeId::of::<T>()).unwrap().borrow_mut(),
-            _marker: PhantomData,
-        }
-    }
-}
+// impl<T: 'static> Deref for ResMut<'_, T> {
+//     type Target = T;
 
-macro_rules! impl_system {
-    ($($params:ident),*) => {
-        #[allow(unused_variables)]
-        #[allow(non_snake_case)]
-        impl<F, $($params : SystemParam + 'static),*> System for FunctionSystem<($($params ,)*), F> 
-            where
-                // for any two arbitrary lifetimes, a mutable reference to F with lifetime 'a
-                // implements FnMut taking parameters of lifetime 'b
-                for<'a, 'b> &'a mut F: 
-                    FnMut($($params),*) +
-                    FnMut($(<$params as SystemParam>::Item<'b>),*)
-        {
-            fn run(&mut self, resources: &mut HashMap<TypeId, RefCell<Box<dyn Any>>>) {
-                // necessary to tell rust exactly which impl to call; it gets a bit confused otherwise
-                fn call_inner<$($params),*>(
-                    mut f: impl FnMut($($params),*),
-                    $(
-                        $params: $params
-                    ),*
-                ) {
-                    f($($params),*)
-                }
+//     fn deref(&self) -> &T {
+//         self.value.downcast_ref().unwrap()
+//     }
+// }
 
-                $(
-                    let $params = $params::retrieve(resources);
-                )*
+// impl<T: 'static> DerefMut for ResMut<'_, T> {
+//     fn deref_mut(&mut self) -> &mut T {
+//         self.value.downcast_mut().unwrap()
+//     }
+// }
 
-                call_inner(&mut self.f, $($params),*)
-            }
-        }
-    };
-}
+// impl<'res, T: 'static> SystemParam for ResMut<'res, T> {
+//     type Item<'new> = ResMut<'new, T>;
 
-macro_rules! impl_into_system {
-    (
-        $($params:ident),*
-    ) => {
-        impl<F, $($params: SystemParam + 'static),*> IntoSystem<($($params,)*)> for F
-            where
-                for<'a, 'b> &'a mut F:
-                    FnMut( $($params),* ) +
-                    FnMut( $(<$params as SystemParam>::Item<'b>),* )
-        {
-            type System = FunctionSystem<($($params,)*), Self>;
+//     fn retrieve<'r>(resources: &'r HashMap<TypeId, RefCell<i32>>) -> Self::Item<'r> {
+//         ResMut {
+//             value: resources.get(&TypeId::of::<T>()).unwrap().borrow_mut(),
+//             _marker: PhantomData,
+//         }
+//     }
+// }
 
-            fn into_system(self) -> Self::System {
-                FunctionSystem {
-                    f: self,
-                    marker: Default::default(),
-                }
-            }
-        }
-    }
-}
+// macro_rules! impl_system {
+//     ($($params:ident),*) => {
+//         #[allow(unused_variables)]
+//         #[allow(non_snake_case)]
+//         impl<F, $($params : SystemParam + 'static),*> System for FunctionSystem<($($params ,)*), F>
+//             where
+//                 // for any two arbitrary lifetimes, a mutable reference to F with lifetime 'a
+//                 // implements FnMut taking parameters of lifetime 'b
+//                 for<'a, 'b> &'a mut F:
+//                     FnMut($($params),*) +
+//                     FnMut($(<$params as SystemParam>::Item<'b>),*)
+//         {
+//             fn run(&mut self, resources: &mut HashMap<TypeId, RefCell<i32>>) {
+//                 // necessary to tell rust exactly which impl to call; it gets a bit confused otherwise
+//                 fn call_inner<$($params),*>(
+//                     mut f: impl FnMut($($params),*),
+//                     $(
+//                         $params: $params
+//                     ),*
+//                 ) {
+//                     f($($params),*)
+//                 }
 
-macro_rules! call_n_times {
-    ($target:ident, 1) => {
-        $target!();
-    };
+//                 $(
+//                     let $params = $params::retrieve(resources);
+//                 )*
 
-    ($target:ident, 2) => {
-        $target!(T1);
-        call_n_times!($target, 1);
-    };
-    
-    ($target:ident, 3) => {
-        $target!(T1, T2);
-        call_n_times!($target, 2);
-    };
-    
-    ($target:ident, 4) => {
-        $target!(T1, T2, T3);
-        call_n_times!($target, 3);
-    };
-    
-    ($target:ident, 5) => {
-        $target!(T1, T2, T3, T4);
-        call_n_times!($target, 4);
-    };
-    
-    ($target:ident, 6) => {
-        $target!(T1, T2, T3, T4, T5);
-        call_n_times!($target, 5);
-    };
-    
-    ($target:ident, 7) => {
-        $target!(T1, T2, T3, T4, T5, T6);
-        call_n_times!($target, 6);
-    };
-    
-    ($target:ident, 8) => {
-        $target!(T1, T2, T3, T4, T5, T6, T7);
-        call_n_times!($target, 7);
-    };
-    
-    ($target:ident, 9) => {
-        $target!(T1, T2, T3, T4, T5, T6, T7, T8);
-        call_n_times!($target, 8);
-    };
-    
-    ($target:ident, 10) => {
-        $target!(T1, T2, T3, T4, T5, T6, T7, T8, T9);
-        call_n_times!($target, 9);
-    };
-}
+//                 call_inner(&mut self.f, $($params),*)
+//             }
+//         }
+//     };
+// }
 
-call_n_times!(impl_system, 10);
-call_n_times!(impl_into_system, 10);
+// macro_rules! impl_into_system {
+//     (
+//         $($params:ident),*
+//     ) => {
+//         impl<F, $($params: SystemParam + 'static),*> IntoSystem<($($params,)*)> for F
+//             where
+//                 for<'a, 'b> &'a mut F:
+//                     FnMut( $($params),* ) +
+//                     FnMut( $(<$params as SystemParam>::Item<'b>),* )
+//         {
+//             type System = FunctionSystem<($($params,)*), Self>;
+
+//             fn into_system(self) -> Self::System {
+//                 FunctionSystem {
+//                     f: self,
+//                     marker: Default::default(),
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// macro_rules! call_n_times {
+//     ($target:ident, 1) => {
+//         $target!();
+//     };
+
+//     ($target:ident, 2) => {
+//         $target!(T1);
+//         call_n_times!($target, 1);
+//     };
+
+//     ($target:ident, 3) => {
+//         $target!(T1, T2);
+//         call_n_times!($target, 2);
+//     };
+
+//     ($target:ident, 4) => {
+//         $target!(T1, T2, T3);
+//         call_n_times!($target, 3);
+//     };
+
+//     ($target:ident, 5) => {
+//         $target!(T1, T2, T3, T4);
+//         call_n_times!($target, 4);
+//     };
+
+//     ($target:ident, 6) => {
+//         $target!(T1, T2, T3, T4, T5);
+//         call_n_times!($target, 5);
+//     };
+
+//     ($target:ident, 7) => {
+//         $target!(T1, T2, T3, T4, T5, T6);
+//         call_n_times!($target, 6);
+//     };
+
+//     ($target:ident, 8) => {
+//         $target!(T1, T2, T3, T4, T5, T6, T7);
+//         call_n_times!($target, 7);
+//     };
+
+//     ($target:ident, 9) => {
+//         $target!(T1, T2, T3, T4, T5, T6, T7, T8);
+//         call_n_times!($target, 8);
+//     };
+
+//     ($target:ident, 10) => {
+//         $target!(T1, T2, T3, T4, T5, T6, T7, T8, T9);
+//         call_n_times!($target, 9);
+//     };
+// }
+
+// call_n_times!(impl_system, 10);
+// call_n_times!(impl_into_system, 10);
 
 mod systems;
 
@@ -222,8 +233,8 @@ fn main() {
     scheduler.add_resource(24usize);
     scheduler.add_resource("Foo");
 
-    #[cfg(not(feature = "call"))]
-    systems::add_systems(&mut scheduler);
+    // #[cfg(not(feature = "call"))]
+    // systems::add_systems(&mut scheduler);
     #[cfg(feature = "call")]
     systems::call_systems(&mut scheduler);
 
